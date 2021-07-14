2610268ab184f7f423f07b3d6ed1a24bfb4cf883
Kai <ebilkal@gmail.com>
2021-07-14 13:40:21 +0800

    updated with new backend model

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <BrowserRouter>
         <Switch>
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/dashboard/portfolio" exact component={Dashboard} />
-            <Route path="/dashboard/watchlist" exact component={Dashboard} />
-            <Route path="/dashboard/details/:symbol" exact component={Dashboard} />
-            <Route path="/dashboard/settings" exact component={Dashboard} />
+            <Route path="/dashboard" component={Dashboard} />
+            {/*<Route path="/dashboard/portfolio" exact component={Dashboard} />*/}
+            {/*<Route path="/dashboard/watchlist" exact component={Dashboard} />*/}
+            {/*<Route path="/dashboard/details/:symbol" exact component={Dashboard} />*/}
+            {/*<Route path="/dashboard/settings" exact component={Dashboard} />*/}
             <Route path="/" exact component={Website}/>
-            <Route path="/market" exact component={Website}/>
-            <Route path="/market/details/:symbol" exact component={Website}/>
+            <Route path="/market"  component={Website}/>
+            {/*<Route path="/market/details/:symbol" exact component={Website}/>*/}
             <Route path="/login" exact component={Website}/>
         </Switch>
     </BrowserRouter>
--- src/components/website/Details.js
+++ src/components/website/Details.js
@@ -12,14 +12,14 @@ function Details({dashboard}) {
     let {symbol} = useParams()
     const [stockDetail, setStockDetail] = useState([])
     const [forecastRecord, setforecastRecord] = useState([])
-    const [historicalRecord, sethistoricalRecord] = useState([])
+    // const [historicalRecord, sethistoricalRecord] = useState([])
 
     useEffect(()=>{
         async function getStock(){
             let {data} = await axios.get(`/show?stock=${symbol}`);
             setStockDetail(data["stock_record"])
             setforecastRecord(data["forecast_record"])
-            sethistoricalRecord(data["historical_record"])
+            // sethistoricalRecord(data["historical_record"])
         }
         getStock()
     },[])
@@ -31,16 +31,14 @@ function Details({dashboard}) {
 
     if(forecastRecord){
 
-        data = forecastRecord.map((element,index) => {
+        data = forecastRecord.map((element) => {
             const dateConvert = new Date(element['date']).toLocaleDateString('en-GB', {  year: 'numeric', month: 'short'})
-            if(index < historicalRecord.length){
-                if(element['date'] === historicalRecord[index].date){
-                    return {
-                        date : dateConvert,
-                        yhat : element['yhat'],
-                        yhat_range : [element["yhat_upper"], element["yhat_lower"]],
-                        price: historicalRecord[index].price
-                    }
+            if(element['price'] !== "0.00"){
+                return {
+                    date : dateConvert,
+                    yhat : element['yhat'],
+                    yhat_range : [element["yhat_upper"], element["yhat_lower"]],
+                    price: element['price']
                 }
             }else{
                 return {
@@ -50,6 +48,8 @@ function Details({dashboard}) {
                 }
             }
         })
+
+        console.log(data)
 
         for (let i in forecastRecord){
 
